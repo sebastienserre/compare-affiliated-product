b9776383ae366d2be870b4bc283e4509f4602650
sebastienserre <sebastien@thivinfo.com>
2018-09-27 19:38:56 +0200

    reformat code

--- classes/class-effiliation.php
+++ classes/class-effiliation.php
@@ -9,53 +9,63 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @since 1.2.0
  * https://publisher.effiliation.com/
  */
-
 class effiliation {
 
+	/**
+	 * @var string Apikey to retrieve in Effiliation.com settings page.
+	 */
 	protected static $apikey;
 
 	public function __construct() {
-		//add_action( 'admin_init', array( $this, 'compare_get_effiliation_program' ) );
 	}
 
 	/**
 	 * @return mixed apikey for effiliation account.
 	 */
-	private function compare_get_apikey(){
-		$option = get_option( 'compare-effiliation' );
+	private static function compare_get_apikey() {
+		$option       = get_option( 'compare-effiliation' );
 		self::$apikey = $option['apikey'];
+
 		return self::$apikey;
 	}
 
-	public static function compare_get_effiliation_program(){
-		$apikey = self::compare_get_apikey();
-		$url = 'https://apiv2.effiliation.com/apiv2/programs.json?key='. $apikey .'&filter=mines';
-		$response = wp_remote_get( $url, array('sslverify'   => false, ) );
+	/**
+	 * @return array array with Effiliation program datas.
+	 */
+	public static function compare_get_effiliation_program() {
+		$apikey   = self::compare_get_apikey();
+		$url      = 'https://apiv2.effiliation.com/apiv2/programs.json?key=' . $apikey . '&filter=mines';
+		$response = wp_remote_get( $url, array( 'sslverify' => false, ) );
 		if ( 200 === (int) wp_remote_retrieve_response_code( $response ) ) {
 			$body         = wp_remote_retrieve_body( $response );
 			$decoded_body = json_decode( $body, true );
 		}
+
 		return $decoded_body;
 	}
 
 	public static function compare_effiliation_list_html() {
 		$programs = self::compare_get_effiliation_program();
-		$options = get_option( 'compare-effiliation' );
+		$options  = get_option( 'compare-effiliation' );
 
-		foreach ( $programs as $program ){
-			foreach ( $program as $prog ){
+		foreach ( $programs as $program ) {
+			foreach ( $program as $prog ) {
 				if ( isset( $options['programs'] ) && ! empty( $options['programs'] ) ) {
 					$check = $options['programs'];
 				}
 				?>
 				<p>
-					<input type="checkbox" value="<?php echo $prog['id_session']; ?>" name="compare-effiliation[programs]" <?php checked( $check, $prog['id_session'])?>>
-					<img src="<?php echo $prog['urllo'];?>">
-					<?php echo $prog['siteannonceur'];?>
+					<input type="checkbox" value="<?php echo $prog['id_session']; ?>"
+					       name="compare-effiliation[programs]" <?php checked( $check, $prog['id_session'] ) ?>>
+					<label for="compare-effiliation[programs]">
+						<img src="<?php echo $prog['urllo']; ?>" alt="<?php echo $prog['siteannonceur']; ?>"
+						     title="<?php echo $prog['siteannonceur']; ?>">
+					</label>
 				</p>
-<?php
+				<?php
 			}
 		}
 	}
 }
+
 new effiliation();
