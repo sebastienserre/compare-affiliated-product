1bbf649ccdb1a6d79611d531c6ccd9f7d0ffb305
sebastienserre <sebastien@thivinfo.com>
2018-10-07 19:11:21 +0200

    load file.php before download_url

--- classes/class-awin.php
+++ classes/class-awin.php
@@ -60,6 +60,7 @@ class Awin {
 	}
 
 	public function compare_get_awin_partners( $partner_code = '' ) {
+		require_once ABSPATH . 'wp-admin/includes/file.php';
 		$url          = 'https://productdata.awin.com/datafeed/list/apikey/' . $this->awin['apikey'];
 		$temp_file    = download_url( $url, 300 );
 		$csv          = file_get_contents( $url );
