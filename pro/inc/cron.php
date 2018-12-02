615b8a62019cec441ad2a3de1682fbcdff51cfdd
sebastienserre <sebastien@thivinfo.com>
2018-12-02 16:09:43 +0100

    run cron only on partners selected

--- pro/inc/cron.php
+++ pro/inc/cron.php
@@ -1,18 +1,27 @@
 <?php
 
-if ( class_exists( 'WP_CLI' ) ){
-	WP_CLI::add_command('cap_import_db', 'cap_upgrade_db' );
+if ( class_exists( 'WP_CLI' ) ) {
+	WP_CLI::add_command( 'cap_import_db', 'cap_upgrade_db' );
 }
 
 function cap_upgrade_db() {
 	error_log( 'start cron' );
 
+	$option = get_option( 'compare-premium' );
 	if ( ! file_exists( 'compare.txt' ) ) {
 		cap_create_pid();
-		$awin = new Awin();
-		$awin->compare_schedule_awin();
-		$effiliation = new Effiliation();
-		$effiliation->compare_schedule_effiliation();
+
+		foreach ( $option['platform'] as $platform ) {
+			if ( 'awin' === $platform ) {
+				$awin = new Awin();
+				$awin->compare_schedule_awin();
+			}
+
+			if ( 'effiliation' === $platform ) {
+				$effiliation = new Effiliation();
+				$effiliation->compare_schedule_effiliation();
+			}
+		}
 		cap_delete_pid();
 
 	} else {
